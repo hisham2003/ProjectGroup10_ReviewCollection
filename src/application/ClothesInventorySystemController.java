1c7c8aaeb3817f3fbe6e61f3cba837c5d641f984
HishamSO <HishamSO@Hisham>
2022-12-04 22:43:54 -0700

    added print outs for totals and abilty to remove from table

--- src/application/ClothesInventorySystemController.java
+++ src/application/ClothesInventorySystemController.java
@@ -2,6 +2,7 @@ package application;
 
 import java.io.IOException;
 import java.net.URL;
+import java.text.DecimalFormat;
 import java.util.ResourceBundle;
 
 import javafx.collections.FXCollections;
@@ -54,6 +55,9 @@ public class ClothesInventorySystemController implements Initializable {
 	private Button addInventory;
 
 	@FXML
+	private Button removeInventory;
+
+	@FXML
 	private TableView<Item> table;
 
 	@FXML
@@ -80,26 +84,72 @@ public class ClothesInventorySystemController implements Initializable {
 	private TableColumn<Clothes, String> materialColumn;
 	@FXML
 	private TableColumn<Clothes, String> sizeColumn;
-	@FXML
-	private Label testLabel;
 
 	@FXML
-	void swicthToAdd(ActionEvent event) throws IOException {
+	private Label totalSaleLabel;
+	@FXML
+	private Label totalHoldLabel;
+	@FXML
+	private Label totalCostLabel;
+    @FXML
+    private Label totalAmountLabel;
+
+	private static final DecimalFormat df = new DecimalFormat("0.00");
+
+	@FXML
+	public void swicthToAdd(ActionEvent event) throws IOException {
 		int idFieldnum = Integer.parseInt(idField.getText());
 		int costFieldnum = Integer.parseInt(costField.getText());
 		int saleFieldnum = Integer.parseInt(saleField.getText());
 		int amountFieldnum = Integer.parseInt(amountField.getText());
 		int totalCostFieldnum = amountFieldnum * costFieldnum;
 		int totalSaleAmountFieldnum = amountFieldnum * saleFieldnum;
+
 		Clothes input = new Clothes(idFieldnum, nameField.getText(), costFieldnum, saleFieldnum, amountFieldnum,
 				totalCostFieldnum, totalSaleAmountFieldnum, sizeField.getText(), materialField.getText());
 		table.getItems().add(input);
 
+		int totalCost = 0;
+		int totalSale = 0;
+		int totalAmount=0;
+		double hold = 0;
+
+		for (Item item : table.getItems()) {
+			totalCost = totalCost + item.getTotalCost();
+			totalSale = totalSale + item.getTotalSalePrice();
+			totalAmount=totalAmount+ item.getAmount();
+			hold = hold + item.getHoldingCostClothes();
+		}
+
+		String totalHold = df.format(hold);
+		totalCostLabel.setText(Integer.toString(totalCost));
+		totalSaleLabel.setText(Integer.toString(totalSale));
+		totalAmountLabel.setText(Integer.toString(totalAmount));
+		totalHoldLabel.setText(totalHold);
 	}
 
-	@FXML
-	void swicthToRemove(ActionEvent event) {
+	// https://stackoverflow.com/questions/34857007/how-to-delete-row-from-table-column-javafx
+	public void removeInventory(ActionEvent event) throws IOException {
+		Item selection = table.getSelectionModel().getSelectedItem();
+		table.getItems().remove(selection);
+		
+		int totalCost = 0;
+		int totalSale = 0;
+		int totalAmount=0;
+		double hold = 0;
 
+		for (Item item : table.getItems()) {
+			totalCost = totalCost + item.getTotalCost();
+			totalSale = totalSale + item.getTotalSalePrice();
+			totalAmount=totalAmount+ item.getAmount();
+			hold = hold + item.getHoldingCostClothes();
+		}
+
+		String totalHold = df.format(hold);
+		totalCostLabel.setText(Integer.toString(totalCost));
+		totalSaleLabel.setText(Integer.toString(totalSale));
+		totalAmountLabel.setText(Integer.toString(totalAmount));
+		totalHoldLabel.setText(totalHold);
 	}
 
 	@Override
@@ -116,12 +166,7 @@ public class ClothesInventorySystemController implements Initializable {
 		totalSalePriceColumn.setCellValueFactory(data -> data.getValue().totalSalePriceProperty().asObject());
 		materialColumn.setCellValueFactory(data -> data.getValue().materialProperty());
 		sizeColumn.setCellValueFactory(data -> data.getValue().sizeProperty());
-		
 
 	}
-
-	
-
-
 
 }
